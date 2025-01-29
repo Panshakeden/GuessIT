1abb0d2782828a7fa8940796c921e2c77eacd1a2
panshak <panshakeden@gmail.com>
2025-01-29 14:17:35 +0100

    GuessIT:loop for inputs

--- src/main.rs
+++ src/main.rs
@@ -5,5 +5,34 @@ fn main() {
     
     println!("Welcome to GuessIT");
 
-    let secret_number = rand::thread_rng().gen_range(1..=50);
+    let secret_number = rand::thread_rng().gen_range(1..=5);
+
+    loop{
+
+        let mut guess = String::new();
+
+        io::stdin().read_line(&mut guess).unwrap();
+
+        let guess :u32 = match guess.trim().parse(){
+            Ok(num) => num,
+            Err(_) =>{
+                println!("Enter number again");
+
+                continue;
+            }
+        };
+
+
+
+        if guess > secret_number{
+            println!("Guess too high");
+        }else if guess < secret_number{
+            println!("Guess too low");
+        }else{
+            println!("Congratulations guess RIGHT!!! {}",secret_number);
+            break;
+        }
+    }
+
+  
 }
