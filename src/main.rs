c807fb28dc5a2ef53bad50e564416d2f5b95b8ca
panshak <panshakeden@gmail.com>
2025-01-30 16:10:26 +0100

    GuessIT: timer and attempt

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,26 @@
 use std::io; 
 use rand::Rng; 
+use std::time::Instant;
 
 fn main() {
     
     println!("Welcome to GuessIT");
 
     let secret_number = rand::thread_rng().gen_range(1..=5);
+    let mut attempt = 0;
+    let total_attempt= 2;
+    let start_time= Instant::now;
 
     loop{
 
+        if attempt >= total_attempt{
+            println!("Game over!!!, Guess better next time,  {} is the number",secret_number);
+
+            break;
+        }
+
+
+        println!("Let's play guessIT, Enter your first Guess");
         let mut guess = String::new();
 
         io::stdin().read_line(&mut guess).unwrap();
@@ -23,13 +35,18 @@ fn main() {
         };
 
 
+         attempt +=1;
 
         if guess > secret_number{
-            println!("Guess too high");
+            println!("Guess too high, {} attempt left",total_attempt- attempt);
         }else if guess < secret_number{
-            println!("Guess too low");
+            println!("Guess too low, {} attempt left", total_attempt - attempt);
         }else{
+
+            let elapsed_time= start_time().elapsed();
+
             println!("Congratulations guess RIGHT!!! {}",secret_number);
+            println!("Time take for the guess after attempts, {}", elapsed_time.as_secs_f64());
             break;
         }
     }
