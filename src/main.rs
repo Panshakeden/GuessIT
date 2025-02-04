856b208d8853173f4509a6ad6242025ad32c4202
panshak <panshakeden@gmail.com>
2025-02-04 14:25:07 +0100

    GuessIT:track scores

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,20 @@
 use std::io; 
 use rand::Rng; 
 use std::time::Instant;
+use std::collections::HashMap;
 
 fn main() {
+
+    println!("Enter your name");
+
+    let mut scores = HashMap::new();
+
+    let mut name = String::new();
+    io::stdin().read_line(&mut name).unwrap();
+
+    let name= name.trim().to_string();
     
-    println!("Welcome to GuessIT");
+    println!("Welcome to GuessIT, {:?}", name);
 
     let secret_number = rand::thread_rng().gen_range(1..=5);
     let mut attempt = 0;
@@ -41,15 +51,18 @@ fn main() {
             println!("Guess too high, {} attempt left",total_attempt- attempt);
         }else if guess < secret_number{
             println!("Guess too low, {} attempt left", total_attempt - attempt);
+
+            scores.insert(name.clone(),attempt);
         }else{
 
             let elapsed_time= start_time().elapsed();
 
             println!("Congratulations guess RIGHT!!! {}",secret_number);
             println!("Time take for the guess after attempts, {}", elapsed_time.as_secs_f64());
+            scores.insert(name.clone(),attempt);
             break;
         }
     }
-
+println!("Your final scores is {:?}", scores)
   
 }
